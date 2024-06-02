e69f29b6459848c60ec5290e695f5aa5ecc79bdb
Miranda-Tang <mirandatang0708@gmail.com>
2024-06-02 20:42:38 +0800

    Add React Router and navigation to application
    The application now includes React Router and a main navigation bar. The "About" and "Home" pages have been created and can be navigated to from this main bar. Additionally, adjustments have been made to font sizes and an additional stylesheet has been linked to improve styling.

--- team-builder/src/App.jsx
+++ team-builder/src/App.jsx
@@ -1,17 +1,31 @@
 import { Provider } from "react-redux";
 import store from "./store";
-import MemberForm from "./MemberForm.jsx";
-import CardDisplay from "./CardDisplay.jsx";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import Home from "./Home.jsx";
+import About from "./About.jsx";
 
 function App() {
   return (
-    <Provider store={store}>
-      <header>Miranda&apos;s Team Builder</header>
-      <div id="main_container">
-        <MemberForm />
-        <CardDisplay />
-      </div>
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Provider>
+    </BrowserRouter>
   );
 }
 
---
+++ team-builder/src/Home.jsx
@@ -0,0 +1,13 @@
+import MemberForm from "./MemberForm.jsx";
+import CardDisplay from "./CardDisplay.jsx";
+
+const Home = () => {
+  return (
+    <div id="main_container">
+      <MemberForm />
+      <CardDisplay />
+    </div>
+  );
+};
+
+export default Home;
